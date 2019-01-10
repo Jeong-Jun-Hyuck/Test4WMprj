5e38700e638d7869c51b4057a7646459d9abd3a1
JeongJunHyuk <wnsgur727@naver.com>
2019-01-10 23:23:03 +0900

    start

---
+++ contact-board1/src/main/java/com/wm/contact/board/ContactBoard1Application.java
@@ -0,0 +1,14 @@
+package com.wm.contact.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBoard1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBoard1Application.class, args);
+	}
+
+}
+
---
+++ contact-board1/src/test/java/com/wm/contact/board/ContactBoard1ApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wm.contact.board;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContactBoard1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
